f7347067e0adb4f2535a56fb7ec06c7def8bac83
Ignacio Vallejos <ignacio.vallejos@genstudents.org>
2025-04-15 20:24:02 -0400

    Entrega tarea classes

--- task2.js
+++ task2.js
@@ -1,6 +1,11 @@
 export class Player {
-  constructor() {
-   
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
-    
+  
+  datos(){
+    console.log(`${this.name} su nivel es ${this.level}`)
   }
+}
+ 
